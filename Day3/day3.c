cd4a1577e997ecc58c7bb093c8ef933208d5370a
Adam Halim <adaab301@student.liu.se>
2020-12-03 15:25:09 +0100

    Finished Day3 part 2
    Moved logic to function and renamed variables. (not a fan of snake_case)

--- Day3/day3.c
+++ Day3/day3.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int treeCounter(int rowJumpIncrementint, int columnJumpIncrement);
+
 int main() {
 
+    printf("Part one: %d\n", treeCounter(3,1));
+    printf("Part two: %ld\n", (treeCounter(1,1) * treeCounter(3,1) * treeCounter(5,1)
+     * treeCounter(7,1) * treeCounter(1,2)));
+
+    return 0;
+}
+
+int treeCounter(int columnJumpIncrement, int rowJumpIncrement) {
 
     // Open our file with input
     FILE *fp;
     fp = fopen("input.txt", "r");
 
-
-    int last_char_pos = 31;
+    int currentX = 0;
+    int treeCounter = 0;
+    int lastCharIndex = 31;
     char line[34];
-    int current_x = 0;
-    int counter = 0;
+    int currentIter = 0;
 
     // We read the file line by line, we increment our index,
     // current_x, by 3 per row, moving us 1 down and 3 right 
     // per loop. 
     while(fgets(line, sizeof(line), fp)) {
-        if(line[current_x % last_char_pos] == '#') {
-            counter++;
+        if(currentIter++  % rowJumpIncrement == 0){
+            if(line[currentX % lastCharIndex] == '#') {
+                treeCounter++;
+            }
+            currentX += columnJumpIncrement;
         }
-        current_x += 3;
     }
-
-    printf("%d\n", counter);
-
-    return 0;
+    return treeCounter;
 }
