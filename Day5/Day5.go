46bd6b542d4189bf6163e08fb15d7c83edccaff4
Adam Halim <adaab301@student.liu.se>
2020-12-06 12:48:52 +0100

    Finished Day5 part 1

---
+++ Day5/Day5.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var line string
+	var rowID int16
+	var highestID int16 = 0
+
+	for scanner.Scan() {
+		line, rowID = getRow(scanner.Text())
+		seatID := getID(line, rowID)
+		if seatID > highestID {
+			highestID = seatID
+		}
+	}
+	fmt.Printf("%d\n", highestID)
+}
+
+func getID(line string, rowID int16) int16 {
+
+	var minCol int16 = 0
+	var maxCol int16 = 7
+
+	for i := 7; i < 10; i++ {
+		if line[i] == 'L' {
+			maxCol = (minCol + maxCol + 1) / 2
+		} else {
+			minCol = (minCol + maxCol) / 2
+		}
+	}
+
+	if line[9] == 'L' {
+		return rowID*8 + minCol
+	}
+	return rowID*8 + maxCol
+}
+
+func getRow(line string) (string, int16) {
+	var minRow int16 = 0
+	var maxRow int16 = 127
+
+	for i := 0; i < 7; i++ {
+		if line[i] == 'F' {
+			maxRow = (minRow + maxRow) / 2
+		} else {
+			minRow = (minRow + maxRow + 1) / 2
+		}
+	}
+	return line, minRow
+}
